1b3a02ac84b4c910bd5e8540340f7cc2812b1ed2
SergejKomnenovic <sergejkomnenovic2003@gmail.com>
2026-02-07 19:06:55 +0100

    Implementing controller for auth api

--- src/app/api/auth/me/route.ts
+++ src/app/api/auth/me/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+/*import { NextRequest, NextResponse } from "next/server";
 import { AUTH_COOKIE, verifyAuthToken } from "@/lib/auth";
 export async function GET(req: NextRequest) {
   try {
@@ -16,4 +16,15 @@ export async function GET(req: NextRequest) {
   } catch {
     return NextResponse.json({ id: null, uloga: null });
   }
+}*/
+import { authController } from "@/app/controllers/authController";
+import { NextRequest } from "next/server";
+
+export const runtime = "edge"; // Optional: koristi Edge runtime da bi NextRequest imao cookies
+
+export async function GET(req: NextRequest) {
+    // Uzmi token iz cookie-ja
+  const token = req.cookies.get("auth_token")?.value; // sada postoji
+    // Pozovi authController.me
+  return authController.me(token);
 }
